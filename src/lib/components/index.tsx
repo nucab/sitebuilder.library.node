e8efa5a02fbd03939af4c662dc92f80d8c5aaa30
Noah John Ucab <noahjohn.ucab@gmail.com>
2018-12-18 10:39:55 +0800

    feat(*): export nodes

--- src/lib/components/index.tsx
+++ src/lib/components/index.tsx
@@ -3,7 +3,17 @@ import RecurseNode from "./RecurseNode";
 import RenderNode from "./RenderNode";
 import HeaderPanel from "./templates/headers/HeaderPanel";
 import FooterPanel from "./templates/footers/FooterPanel";
+import Section from "./templates/Section";
+import Carousel from "./templates/Carousel";
 
-export { Node, RecurseNode, RenderNode, HeaderPanel, FooterPanel };
+export {
+  Node,
+  RecurseNode,
+  RenderNode,
+  HeaderPanel,
+  FooterPanel,
+  Section,
+  Carousel
+};
 
 export default Node;
