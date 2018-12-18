98e7fb027216e7093ed5dbaf8b32b332d39fa3f1
Noah John Ucab <noahjohn.ucab@gmail.com>
2018-12-18 13:02:10 +0800

    fix(*): entry index

--- src/lib/index.tsx
+++ src/lib/index.tsx
@@ -3,7 +3,17 @@ import RecurseNode from "./components/RecurseNode";
 import RenderNode from "./components/RenderNode";
 import HeaderPanel from "./components/templates/headers/HeaderPanel";
 import FooterPanel from "./components/templates/footers/FooterPanel";
+import Section from "./components/templates/Section";
+import Carousel from "./components/templates/Carousel";
 
-export { Node, RecurseNode, RenderNode, HeaderPanel, FooterPanel };
+export {
+  Node,
+  RecurseNode,
+  RenderNode,
+  HeaderPanel,
+  FooterPanel,
+  Section,
+  Carousel
+};
 
 export default Node;
