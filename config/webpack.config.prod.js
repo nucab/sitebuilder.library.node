c9f5a726fdf89bd3c097d5af1702ed78432297b5
Noah John Ucab <noahjohn.ucab@gmail.com>
2018-12-18 16:34:44 +0800

    fix(*): remove chunking of scripts

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -183,14 +183,14 @@ module.exports = {
             : false
         }
       })
-    ],
+    ]
     // Automatically split vendor and commons
     // https://twitter.com/wSokra/status/969633336732905474
     // https://medium.com/webpack/webpack-4-code-splitting-chunk-graph-and-the-splitchunks-optimization-be739a861366
-    splitChunks: {
-      chunks: "all",
-      name: false
-    }
+    // splitChunks: {
+    //   chunks: "all",
+    //   name: false
+    // }
   },
   resolve: {
     // This allows you to set a fallback for where Webpack should look for modules.
