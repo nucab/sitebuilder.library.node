fb063803d94a2a5035da89c5d659a90452a540ba
Noah John Ucab <noahjohn.ucab@gmail.com>
2018-12-18 15:13:58 +0800

    fix(*): re-add Node

--- src/lib/index.tsx
+++ src/lib/index.tsx
@@ -1,6 +1,9 @@
+import Node from "./components/Node";
 import HeaderPanel from "./components/templates/headers/HeaderPanel";
 import FooterPanel from "./components/templates/footers/FooterPanel";
 import Section from "./components/templates/Section";
 import Carousel from "./components/templates/Carousel";
 
-export { HeaderPanel, FooterPanel, Section, Carousel };
+export { Node, HeaderPanel, FooterPanel, Section, Carousel };
+
+export default Node;
